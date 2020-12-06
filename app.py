e836ffb78d87a1b61040e4862dca4fb287f23f82
ciskander <75549366+ciskander@users.noreply.github.com>
2020-12-06 18:47:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,22 @@ from flask import request
 app = Flask(__name__)
 
 
-def createNotionTask(token, collectionURL, todo.title, todo.note):
+def createNotionTask(token, collectionURL, title, note):
     # notion
     client = NotionClient(token)
     cv = client.get_collection_view(collectionURL)
     row = cv.collection.add_row()
-    row.title = todo.title
-	row.note = todo.note
+    row.title = title
+	row.note = note
 
 
 @app.route('/create_todo', methods=['GET'])
 def create_todo():
-    todo.title = request.args.get('title')
-	todo.note = request.args.get('note')
+    title = request.args.get('title')
+	note = request.args.get('note')
     token_v2 = os.environ.get("TOKEN")
     url = os.environ.get("URL")
-    createNotionTask(token_v2, url, todo.title, todo.note)
+    createNotionTask(token_v2, url, title, note)
     return f'added {todo} to Notion'
 
 
